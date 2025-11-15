819829f0e0c744450233a453c4f064c8c5c9b90f
adrianomabote <maboteadriano5@gmail.com>
2025-11-15 11:50:35 +0200

    fix: remove imagem inexistente do signup e ajusta App

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -6,7 +6,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { ArrowLeft } from 'lucide-react';
 import { useProtection } from '@/hooks/use-protection';
-import hackerImage from '@assets/IMG-20251105-WA0139_1762343129352.jpg';
 
 export default function Signup() {
   useProtection();
@@ -63,7 +62,7 @@ export default function Signup() {
             </Link>
             <div className="flex items-center gap-1.5">
               <img
-                src={hackerImage}
+                src={"/favicon.png"}
                 alt="CyberHacker"
                 className="w-5 h-5 rounded-lg object-cover border-2 border-red-600"
               />
